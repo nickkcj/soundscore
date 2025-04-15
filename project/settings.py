233a12e1f4c5b5a7a3f335a748fb87a533be5f76
Nickkcj <nickcj123@hotmail.com>
2025-04-14 23:10:22 -0300

    add: renderstaticfiles

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
